39ee59e86d465547a21af11287017c2544542ccd
Maverick Tesini <mavericktesini@gmail.com>
2018-03-05 18:39:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 #this method uses a range between 2 and half a given number.
 #The method checks if none?(true) iteration returns true.
 #if so, then only 1 and the given number divide perfectly into the given number.
-#hence, the definition of a prime number. 
+#hence, the definition of a prime number.
 #Any other number less than 2, by default isn't prime.
 def prime?(n)
   if n < 2
