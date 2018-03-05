498f50117da83e7661749284df4f5b8f1c99ee19
Maverick Tesini <mavericktesini@gmail.com>
2018-03-05 18:37:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(n)
   if n < 2
     return false
-  elsif (2..n/2).none?{|i| n % i == 0}
+  elsif (2..n/2).none?{|i| n % i == 0}  #between 2 and (argument/2), if no numbers are whole dividers (no remainders), then only 1 and self will divide... definition of a prime.
     return true
   else
     false
