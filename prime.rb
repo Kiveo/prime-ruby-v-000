3a4e066bf816a096f790238e0045abd8ecdfe85f
Maverick Tesini <mavericktesini@gmail.com>
2018-03-05 18:39:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,9 @@
 # Add  code here!
+#this method uses a range between 2 and half a given number.
+#The method checks if none?(true) iteration returns true.
+#if so, then only 1 and the given number divide perfectly into the given number.
+#hence, the definition of a prime number. 
+#Any other number less than 2, by default isn't prime.
 def prime?(n)
   if n < 2
     return false
